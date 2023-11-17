cc219412d27b1c3a0d17990e21037ad2a327e9ee
ReZvanYY <141616463+ReZvanYY@users.noreply.github.com>
2023-11-17 14:26:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace basic testing
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hva heter du?");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Hei {name}!");
+
+        Console.WriteLine("hvor gammel er du?");
+        var age = Console.ReadLine();
+        Console.WriteLine($"Du er {age} år gammel!");
+
+        Console.WriteLine("Hvor bor du");
+        var location = Console.ReadLine();
+        Console.WriteLine($"Du bor på {location}");
+
+        Console.WriteLine("Hvilket hobby har du?");
+        var hobbies = Console.ReadLine();
+        Console.WriteLine($"Du liker {hobbies}!");
+    }
+
+}
+
+}
